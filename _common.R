d564548dce3b9144b516a457e629da36dfd208ce
hietalai <isak.hietala@liu.se>
2024-09-10 13:03:10 +0200

    Adding programming introduction, videos to majority of chapters.

--- _common.R
+++ _common.R
@@ -21,4 +21,42 @@ modelData <-
 
 simpleModel <- stats::lm(formula = bill_length_mm ~ ., data = modelData)
 
-options(knitr.kable.NA = '')
+options(knitr.kable.NA = '', knitr.table.html.attr = "quarto-disable-processing=true")
+
+
+residualPlots <- function(model) {
+  
+  residualData <- 
+    data.frame(
+      residuals = residuals(model),
+      # Responsvariabeln finns som första kolumn i modellens model-objekt
+      y = model$model[,1],
+      yHat = fitted(model)
+    )
+  
+  
+  p1 <- ggplot2::ggplot(residualData) + 
+    ggplot2::aes(x = residuals, y = after_stat(density)) +
+    ggplot2::geom_histogram(bins = 20, fill = "steelblue", color = "black") + 
+    ggplot2::theme_bw() + 
+    ggplot2::labs(x = "Residualer", y = "Densitet")
+  
+  p2 <- ggplot2::ggplot(residualData) + 
+    ggplot2::aes(x = yHat, y = residuals) + 
+    ggplot2::geom_hline(aes(yintercept = 0)) + 
+    ggplot2::geom_point(color = "steelblue") + 
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = "Anpassade värden", y = "Residualer")
+  
+  
+  p3 <- ggplot2::ggplot(residualData) + 
+    # Använder standardiserade residualer
+    ggplot2::aes(sample = scale(residuals)) + 
+    ggplot2::geom_qq_line() + 
+    ggplot2::geom_qq(color = "steelblue") +
+    ggplot2::theme_bw() + 
+    ggplot2::labs(x= "Teoretiska kvantiler", y = "Observerade kvantiler")
+  
+  cowplot::plot_grid(p1, p2, p3, nrow = 2)
+  
+}
